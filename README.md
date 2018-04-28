f18b0a4cd4ef44d8736afe17ed18aafefb59eac3
Daniele Demichelis <escher75@gmail.com>
2018-04-28 21:22:00 +0000

    REST client as SpringBoot App

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tutorial-microservices
+
 Tutorials about microservices
+
+## References
+
+__Spring Boot__
+
+Full doc: [https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/]
--- microservices-spring-boot-rest-client/src/main/java/com/danidemi/tutorial/microservices/springboot/restclient/RestClient.java
+++ microservices-spring-boot-rest-client/src/main/java/com/danidemi/tutorial/microservices/springboot/restclient/RestClient.java
@@ -1,22 +1,39 @@
 package com.danidemi.tutorial.microservices.springboot.restclient;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-public class RestClient {
-
+@SpringBootApplication
+public class RestClient implements CommandLineRunner {
+	
     public static void main(String[] args) {
-        new RestClient().run();
+    	
+    	// It's maybe overkill to use Spring Boot to run a simple
+    	// REST client, but the goal of this example is to show how to 
+    	// invoke an external REST service from within a Spring Boot app.
+    	
+    	SpringApplication application = new SpringApplication(RestClient.class);
+    	application.setWebApplicationType(WebApplicationType.NONE);
+    	application.run(args);
     }
 
-    private void run() {
-        RestTemplate restTemplate = new RestTemplate();
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// This rest template based on Apache's HTTP Components
+		// allows to have a full log of the HTTP traffic.
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
 
         HttpEntity<NewItem> request = new HttpEntity<>(new NewItem("bar"));
-        Item foo = restTemplate.postForObject("http://localhost:8080/items", request, Item.class);
+        Item foo = restTemplate.postForObject("http://localhost:9090/items", request, Item.class);
 
         System.out.println(foo);
-
-    }
+        		
+	}
 
 }
