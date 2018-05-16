2ccc4eace654cd9919f1fe5c1c65644f647f7b9e
Daniele Demichelis <d.demichelis@techgap.it>
2018-05-16 18:45:52 +0200

    Renamed eureka based services.

--- eureka-discoverable/src/main/java/ms/eureka/discoverable/DiscoverableInEurekaApp.java
+++ eureka-discoverable/src/main/java/ms/eureka/discoverable/DiscoverableInEurekaApp.java
@@ -1,16 +1,14 @@
-package ms.eureka.client;
+package ms.eureka.discoverable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,7 +19,7 @@ import java.util.List;
  */
 @SpringBootApplication
 @RestController
-public class AppWithEurekaClient {
+public class DiscoverableInEurekaApp {
 
     @Autowired
     Environment env;
@@ -44,8 +42,7 @@ public class AppWithEurekaClient {
     }
 
     public static void main(String[] args) {
-        //new SpringApplicationBuilder(AppWithEurekaClient.class).web(true).run(args);
-        SpringApplication.run(AppWithEurekaClient.class, args);
+        SpringApplication.run(DiscoverableInEurekaApp.class, args);
     }
 
 }
